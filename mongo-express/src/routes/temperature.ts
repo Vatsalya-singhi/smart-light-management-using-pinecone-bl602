ebeee1e0e361f5eb67eaafb5b98525162db34134
Vatsalya-singhi <vatsalya.singhi4316@gmail.com>
2023-11-07 16:58:15 +0100

    express app update

--- mongo-express/src/routes/temperature.ts
+++ mongo-express/src/routes/temperature.ts
@@ -3,17 +3,16 @@ import { conn, db } from '../db_conn';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const temperature_collection = process.env.MONGODB_COLLECTION_TEMPERATURE;
+const collection = process.env.MONGODB_COLLECTION_TEMPERATURE;
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    let results = await db.collection(temperature_collection)
+    let results = await db.collection(collection)
         .find({})
         .limit(50)
         .toArray();
     res.send(results).status(200);
-    // res.send("temperature page");
 });
 
 export default router;
