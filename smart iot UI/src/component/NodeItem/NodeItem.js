53cafafb9c0929357a8cbb1c25232d6bd5d96c49
Vatsalya-singhi <vatsalya.singhi4316@gmail.com>
2024-01-03 12:47:25 +0530

    chartjs init

--- smart iot UI/src/component/NodeItem/NodeItem.js
+++ smart iot UI/src/component/NodeItem/NodeItem.js
@@ -18,6 +18,13 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 
 import MQTT from 'mqtt';
 
+import { Chart as ChartJS, defaults } from "chart.js/auto";
+import { Bar, Doughnut, Line } from "react-chartjs-2";
+import "./NodeItem.css";
+import revenueData from "../../assets/data/revenueData.json";
+import sourceData from "../../assets/data/sourceData.json";
+
+
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
     ...theme.typography.body2,
@@ -91,6 +98,15 @@ const CustomThumb = () => (
         />
     </svg>
 );
+
+
+defaults.maintainAspectRatio = false;
+defaults.responsive = true;
+
+defaults.plugins.title.display = true;
+defaults.plugins.title.align = "start";
+defaults.plugins.title.font.size = 20;
+defaults.plugins.title.color = "black";
 
 // enum LED_STATUS { ON, OFF, AUTO, }
 // ON = 0
@@ -356,6 +372,102 @@ function NodeItem() {
                     </Item>
                 </Grid>
             </Grid>
+
+
+            <div className="App">
+                <div className="dataCard revenueCard">
+                    <Line
+                        data={{
+                            labels: revenueData.map((data) => data.label),
+                            datasets: [
+                                {
+                                    label: "Revenue",
+                                    data: revenueData.map((data) => data.revenue),
+                                    backgroundColor: "#064FF0",
+                                    borderColor: "#064FF0",
+                                },
+                                {
+                                    label: "Cost",
+                                    data: revenueData.map((data) => data.cost),
+                                    backgroundColor: "#FF3030",
+                                    borderColor: "#FF3030",
+                                },
+                            ],
+                        }}
+                        options={{
+                            elements: {
+                                line: {
+                                    tension: 0.5,
+                                },
+                            },
+                            plugins: {
+                                title: {
+                                    text: "Monthly Revenue & Cost",
+                                },
+                            },
+                        }}
+                    />
+                </div>
+
+                <div className="dataCard customerCard">
+                    <Bar
+                        data={{
+                            labels: sourceData.map((data) => data.label),
+                            datasets: [
+                                {
+                                    label: "Count",
+                                    data: sourceData.map((data) => data.value),
+                                    backgroundColor: [
+                                        "rgba(43, 63, 229, 0.8)",
+                                        "rgba(250, 192, 19, 0.8)",
+                                        "rgba(253, 135, 135, 0.8)",
+                                    ],
+                                    borderRadius: 5,
+                                },
+                            ],
+                        }}
+                        options={{
+                            plugins: {
+                                title: {
+                                    text: "Revenue Source",
+                                },
+                            },
+                        }}
+                    />
+                </div>
+
+                <div className="dataCard categoryCard">
+                    <Doughnut
+                        data={{
+                            labels: sourceData.map((data) => data.label),
+                            datasets: [
+                                {
+                                    label: "Count",
+                                    data: sourceData.map((data) => data.value),
+                                    backgroundColor: [
+                                        "rgba(43, 63, 229, 0.8)",
+                                        "rgba(250, 192, 19, 0.8)",
+                                        "rgba(253, 135, 135, 0.8)",
+                                    ],
+                                    borderColor: [
+                                        "rgba(43, 63, 229, 0.8)",
+                                        "rgba(250, 192, 19, 0.8)",
+                                        "rgba(253, 135, 135, 0.8)",
+                                    ],
+                                },
+                            ],
+                        }}
+                        options={{
+                            plugins: {
+                                title: {
+                                    text: "Revenue Sources",
+                                },
+                            },
+                        }}
+                    />
+                </div>
+            </div>
+
         </>
     );
 }
